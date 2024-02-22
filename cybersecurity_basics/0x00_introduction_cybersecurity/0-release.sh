b2cc055a2518c7638595b7b824fc7cc4fe8fa18e
MedJarraya <med.jarraya.02@gmail.com>
2024-02-22 16:56:59 -0500

    fix

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,8 +1,2 @@
-# Get distributor ID
-distributor_id=$(lsb_release -si 2>/dev/null)
-
-if [ $? -eq 0 ]; then
-    echo $distributor_id
-else
-    echo "lsb_release command not found. Unable to determine Distributor ID."
-fi
+#!/bin/bash
+echo $(lsb_release -si 2>/dev/null)
