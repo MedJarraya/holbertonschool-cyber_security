5dff575b59307a75cc94a3c3c30e32a8ed9a75ed
MedJarraya <med.jarraya.02@gmail.com>
2024-02-25 08:56:00 -0500

    1

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [[ "$#" -ne 1 ]]; then
-    echo "Usage: $0 <username>"
-    exit 1
-fi
-
-user=$1
-ps aux | grep "^$user" | awk '$6 > 0 && $7 > 0 {print $0}'
+[ "$#" -eq 1 ] && ps -u "$1" -o pid,vsz,rss,args | awk '$2 > 0 && $3 > 0 {print $0}'
