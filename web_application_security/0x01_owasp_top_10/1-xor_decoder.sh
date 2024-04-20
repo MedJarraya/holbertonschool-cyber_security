ea8b049b4d0dcbb89f10f3728bd26d9e84678ba5
MedJarraya <med.jarraya.02@gmail.com>
2024-04-20 07:26:08 -0400

    new

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Check if an argument is provided
+# This script is a decoder
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <encoded_string>"
     exit 1
 fi
 
-# Decode XOR-encoded string
+
 password="${1#'{xor}'}"
 decoded_password=$(echo -n "$password" | base64 -d | tr -d '\0' | tr -dc '\11\12\15\40-\176')
 
-# XOR each character with the ASCII value of '_'
+
 decoded_password_xor=""
 for ((i = 0; i < ${#decoded_password}; i++)); do
     char="${decoded_password:$i:1}"
