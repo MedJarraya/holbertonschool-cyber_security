db5db6becd38cd7562c060f92ba7f4c8907db629
MedJarraya <med.jarraya.02@gmail.com>
2024-04-20 07:28:00 -0400

    new

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-password="$1"
-password="${password#'{xor}'}"
-decoded_password=$(echo "$password" | base64 -d)
+# This script is a decoder
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <encoded_string>"
+    exit 1
+fi
 
-#Mot de passe décodé : +:,+
 
-# XOR chaque caractère avec la valeur ASCII de '_'
+password="${1#'{xor}'}"
+decoded_password=$(echo -n "$password" | base64 -d | tr -d '\0' | tr -dc '\11\12\15\40-\176')
+
+
 decoded_password_xor=""
 for ((i = 0; i < ${#decoded_password}; i++)); do
-    char="${decoded_password:$i:1}" # decoded_password[i] de length 1
-    ascii_value=$(printf "%d" "'$char") # convertir le caractère en valeur ASCII décimal at l'attribuer à la variable ascii_value
-    xor_result=$(( ascii_value ^ 95 )) # decode le caractère avec la valeur ASCII de '_' en binaire
-    decoded_password_xor+="$(printf "$(printf '\\x%x' $xor_result)")" #Version avec conversion en hexadécimal
-    # decoded_password_xor+="$(printf "\\$(printf '%03o' $xor_result)")" #Version avec conversion en octal
-    
+    char="${decoded_password:$i:1}"
+    ascii_value=$(printf "%d" "'$char")
+    xor_result=$(( ascii_value ^ 95 ))
+    decoded_password_xor+="$(printf "\\x$(printf '%x' $xor_result)")"
 done
-echo "$decoded_password_xor"
+
+echo -n "$decoded_password_xor"
